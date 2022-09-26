c3c49daeed87de8bf9d65137ccde8022a62f6d3b
vyura06 <vechorko.yura@gmail.com>
2022-09-26 16:59:47 +0300

    Update server

--- server.js
+++ server.js
@@ -17,7 +17,48 @@ const mimeLookup = {
   ".js": "application/javascript",
 };
 
+const staticLocation = "./client/";
+const getFileUrl = url => (url === "/" ? "index.html" : url);
+const getFilePath = fileUrl => path.resolve(staticLocation + fileUrl);
 
+const getNewPage = url => {
+  const myURL = new URL(url, "http://sample.url/");
+
+  const page = +myURL.searchParams.get("page");
+  const seed = +myURL.searchParams.get("seed");
+  const locale = myURL.searchParams.get("locale");
+  const errors = +myURL.searchParams.get("errors");
+
+  setLocale(locale);
+  setSeed(page + seed);
+  setErrors(errors);
+
+  if (page === 1)
+    fakeData = [];
+
+  const data = getNextPage(page);
+  fakeData = fakeData.concat(data);
+
+  return data;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET") {
+    if (req.url.startsWith("/data?")) {
+      const response = getNewPage(req.url);
+      res.end(JSON.stringify(response));
+      return;
+    } 
+
+    const filePath = getFilePath(getFileUrl(req.url));
+    const mimeType = mimeLookup[path.extname(filePath)];
+
+    if (!mimeType) return;
+
+    res.writeHead(200, { "Content-Type": mimeType });
+    fs.createReadStream(filePath).pipe(res);
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
