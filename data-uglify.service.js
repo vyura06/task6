09d290d641a43633bd967ea400070c52ab0bd1ed
vyura06 <vechorko.yura@gmail.com>
2022-09-26 17:58:30 +0300

    Added uglify

---
+++ data-uglify.service.js
@@ -0,0 +1,67 @@
+"use strict";
+
+class UglifyService {
+  constructor() {
+    this.errorsCount = 0;
+    this.remainder = 0;
+    this.possibleError = 0;
+    this.randomizeFn = Math.random;
+    this.uglifyMethods = [
+      this.addRandomSymbol,
+      this.deleteRandomSymbol,
+      this.swapNearSymbols
+    ];
+
+    this.symbols = [].concat(
+      getCharsByCodes(48, 57),
+      getCharsByCodes(65, 90),
+      getCharsByCodes(97, 122),
+    );
+  }
+
+  set ErrorsCount(realNumber) {
+    this.errorsCount = Math.floor(realNumber);
+    this.remainder = realNumber - this.errorsCount;
+  }
+
+  set PossibleError(probability) {
+    this.possibleError = probability < this.remainder ? 1 : 0;
+  }
+
+  set Locale(locale) {
+    this.symbols = [].concat(getCharsByCodes(48, 57));
+
+    if (locale === "ru") {
+      this.symbols = [].concat(
+        getCharsByCodes(1040, 1103),
+      );
+    } else {
+      this.symbols = [].concat(
+        getCharsByCodes(65, 90),
+        getCharsByCodes(97, 122),
+      );
+    }
+  }
+
+  addRandomSymbol(strArr, index) {
+    const symbolToAdd =
+      this.symbols[this.randomizeFn(this.symbols.length, 0)];
+
+    strArr.splice(index, 0, symbolToAdd);
+    return strArr;
+  }
+
+  deleteRandomSymbol(strArr, index) {
+    strArr.splice(index, 1);
+    return strArr;
+  }
+
+  swapNearSymbols(strArr, index) {
+    const nearSymbols = strArr.splice(index, 2).reverse();
+    strArr.splice(index, 0, ...nearSymbols);
+    return strArr;
+  }
+
+  
+
+module.exports = new UglifyService();
